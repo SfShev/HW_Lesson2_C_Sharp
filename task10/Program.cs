5c1dc7778213f9ba82d781a1ab3ddb46ae1a9c4b
SfShev <pshev1406@gmail.com>
2022-06-26 19:42:16 +0300

    Показывает вторую цифру трехзначного числа

---
+++ task10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Если хотите узнать какая вторая цифра этого трехзначного числа");
+Console.WriteLine("Введите трехзначное число");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int a = number1 % 100;
+int b = a / 10;
+
+if (number1 > 99 && number1 < 999)
+{
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Неверное число");
+}
